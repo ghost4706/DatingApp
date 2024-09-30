be694713785b9f659dae8d3ec6d5bb35d90b6253
ghost4706 <kapilpandey06@gmail.com>
2024-09-30 09:23:07 +0530

    Angular commit

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt=>{
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
+
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +26,8 @@ var app = builder.Build();
 // app.UseHttpsRedirection();
 
 // app.UseAuthorization();
-
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
