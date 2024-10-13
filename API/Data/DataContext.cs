bfb8f1e085c64992dc9a78a083febf0f9c5d1a78
ghost4706 <kapilpandey06@gmail.com>
2024-10-13 09:36:02 +0530

    End of Section 15

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,6 +10,8 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 
     public DbSet<UserLike> Likes { get; set; }
 
+    public DbSet<Message> Messages { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -29,6 +31,16 @@ public class DataContext(DbContextOptions options) : DbContext(options)
                 .HasOne(s => s.TargetUser)
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.TargetUserId)
-                .OnDelete(DeleteBehavior.Cascade);                
+                .OnDelete(DeleteBehavior.Cascade);   
+
+        builder.Entity<Message>()
+            .HasOne(x => x.Recipient)
+            .WithMany(x => x.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Message>()
+            .HasOne(x => x.Sender)
+            .WithMany(x => x.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);                     
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -25,4 +25,6 @@ public class AppUser
 
     public List<UserLike> LikedByUsers { get; set; } = [];
     public List<UserLike> LikedUsers { get; set; } = [];
+    public List<Message> MessagesSent { get; set; } = [];
+    public List<Message> MessagesReceived { get; set; } = [];
 }
